3e114297c6c726e9b6572d9c0be43a345bdfbc5e
piotrwalatjimp <36878039+piotrwalatjimp@users.noreply.github.com>
2018-10-19 14:55:01 +0200

    #2
    po lab3

--- Java_obiektowe/Obiektowe/src/lab1/Integer.java
+++ Java_obiektowe/Obiektowe/src/lab1/Integer.java
@@ -1,5 +1,82 @@
 package lab1;
 
-public class Integer {
+
+public class IntegerValue extends Value{
+	int i;
+	
+	IntegerValue (int x)
+	{
+		i=x;
+	}
+	
+	public String toString()
+	{
+		return String.valueOf(i);
+	}
+	
+    public Value add(IntegerValue v)
+    {
+    	return (new IntegerValue(i+v.i));
+    }
+    
+    public Value sub(IntegerValue v)
+    {
+    	return (new IntegerValue(i-v.i));
+    }
+    
+    public Value mul(IntegerValue v)
+    {
+    	return (new IntegerValue(i*v.i));
+    }
+    
+    public Value div(IntegerValue v)
+    {
+    	if(v.i!=0){
+    		return (new IntegerValue(i/v.i));
+    	}
+    	else
+    		throw (new IllegalArgumentException("Dear God..."));
+    }
+    
+    public Value pow(IntegerValue v)
+    {
+    	return (new IntegerValue((int)Math.pow(i, v.i)));
+    }
+    
+    public boolean  eq(IntegerValue v)
+    {
+    	return v.i==i;
+    }
+    
+    public boolean lte(IntegerValue v)
+    {
+    	return v.i>=i;
+    }
+    
+    public boolean gte(IntegerValue v)
+    {
+    	return v.i<=i;
+    }
+    
+    public boolean neq(IntegerValue v)
+    {
+    	return v.i!=i;
+    }
+    
+    public boolean equals(Object other)
+    {
+    	
+    }
+    
+    public int hashCode()
+    {
+    	
+    }
+    
+    public Value create(String s)
+    {
+    	int tmp=Integer.parseInt(s);
+    	return (new	IntegerValue(tmp));
+    }
 
 }
--- Java_obiektowe/Obiektowe/src/lab1/Value.java
+++ Java_obiektowe/Obiektowe/src/lab1/Value.java
@@ -9,11 +9,11 @@ public abstract class Value
     public abstract Value mul(Value v);
     public abstract Value div(Value v);
     public abstract Value pow(Value v);
-    public abstract Value eq(Value v);
-    public abstract Value lte(Value v);
-    public abstract Value gte(Value v);
+    public abstract boolean eq(Value v);
+    public abstract boolean lte(Value v);
+    public abstract boolean gte(Value v);
     public abstract boolean neq(Value v);
-    //public abstract boolean equals(Object other);
-    //public abstract int hashCode(); //dokumentacja Javy - wykład!
+    public abstract boolean equals(Object other);
+    public abstract int hashCode(); //dokumentacja Javy - wykład!
     public abstract Value create(String s);
 }
