9aecd371e8ef19410e4252a688a525f2233bfc81
piotrwalatjimp <36878039+piotrwalatjimp@users.noreply.github.com>
2018-10-19 14:54:11 +0200

    #1
    po lab3

--- Java_obiektowe/Obiektowe/src/lab1/Value.java
+++ Java_obiektowe/Obiektowe/src/lab1/Value.java
@@ -6,11 +6,14 @@ public abstract class Value
     public abstract String toString();
     public abstract Value add(Value v);
     public abstract Value sub(Value v);
+    public abstract Value mul(Value v);
+    public abstract Value div(Value v);
+    public abstract Value pow(Value v);
     public abstract Value eq(Value v);
     public abstract Value lte(Value v);
     public abstract Value gte(Value v);
-    public abstract Value neq(Value v);
-    public abstract boolean equals(Object other);
-    public abstract int hashCode();
-    public abstract Object create(String s);
+    public abstract boolean neq(Value v);
+    //public abstract boolean equals(Object other);
+    //public abstract int hashCode(); //dokumentacja Javy - wykład!
+    public abstract Value create(String s);
 }
